830a407336028afc6d1361dc439c11c08ab91556
AReeme <areeme@student.neumont.edu>
2024-01-31 13:57:41 -0700

    UI For All Screens Completed

---
+++ Pente/PVP.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pente
+{
+    /// <summary>
+    /// Interaction logic for PVP.xaml
+    /// </summary>
+    public partial class PVP : Window
+    {
+        public PVP()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
